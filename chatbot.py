9a870318ba883a41ce56673e6450cba884f95263
Wilson Andres Bachiller Ortiz <32311390+wabox32@users.noreply.github.com>
2022-11-07 18:33:55 -0500

    se realiza ajuste para webhook en php

--- chatbot.py
+++ chatbot.py
@@ -12,9 +12,6 @@ from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 
-
-
-
 def clean_up_sentece(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
@@ -32,13 +29,18 @@ def bag_of_words(sentence, name_chatbot):
 
 def predict_class(filename, sentence, name_chatbot):
     classes = pickle.load(open('classes/'+name_chatbot+'-classes.pkl', 'rb'))
+
     model = load_model('models/'+name_chatbot+'.model')
+
+
     bow = bag_of_words(sentence, name_chatbot)
+
     res = model.predict(np.array([bow]))[0]
     ERROR_TRESHOLD = 0.25
 
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_TRESHOLD]
     results.sort(key= lambda x: x[1], reverse=True)
+
     return_list = []
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
@@ -47,9 +49,13 @@ def predict_class(filename, sentence, name_chatbot):
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
+
     for i in list_of_intents:
         if i['tag'] == tag:
-            result = random.choice(i['responses'])
+            result = {}
+            result['items'] = i
+            result['items']['responses'] = random.choice(i['responses'])
+            result['items'].pop('patterns')
             break
     return result
 
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 #from bson.json_util import dumps, ObjectId
 
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask import jsonify
 from training import *
 from chatbot import *
 from firebase import firebase
 import json
+from flask_cors import CORS, cross_origin
+import requests
 
 
 
 firebase = firebase.FirebaseApplication("https://chatbot-ba490-default-rtdb.firebaseio.com/", None)
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/api": {"origins": "http://localhost:4200"}})
+
+
+
 
 @app.route('/api/v1/intents',  methods=['GET'])
 def index():
@@ -46,10 +53,27 @@ def load(id):
     return jsonify({'success':  200, "message": "services ok" })
 
 @app.route('/api/v1/intents/converstion/<id>',  methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def initials(id):
-    response = firebase.get('/intents', '')
-    data = json.dumps(response[id])
+    firebasejson = firebase.get('/intents', '')
+    data = json.dumps(firebasejson[id])
     jsondata = request.get_json(force=True)
-    respusta = conversation(data, jsondata['message'], id)
-    return jsonify({'success':  200, "message": "services ok", "data" : {"response": respusta, "question": jsondata['message'] } })
+    if jsondata['next_param'].lower() == '':
+        message = jsondata['message'].lower()
+        respuesta = conversation(data, jsondata['message'].lower(), id)
+    else:
+        message = jsondata['next_param'].lower()
+        respuesta = conversation(data, jsondata['next_param'].lower(), id)
+        print(respuesta['items']['webhook'])
+        if respuesta['items']['webhook']:
+            res = requests.post(f"http://inventario.test/api/v1/{respuesta['items']['url']}", json={'message': jsondata['message'].lower()})
+            como_json = res.json()
+            respuesta['items']['type']  = como_json['type']
+            respuesta['items']['options']  = como_json['options']
+            respuesta['items']['responses'] = como_json['message']
+            respuesta['items']['next_patterns'] = como_json['next_patterns']
+
+
+
+    return jsonify({'success': 200, "message": "services ok", "data": {"response": respuesta, "question": jsondata['message']}})
 
